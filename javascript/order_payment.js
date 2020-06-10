6b82368aa938eae1ed4d250d57465f2e7c86f1d5
woniyoon <gellifisch@gmail.com>
2020-06-10 21:43:00 +0900

    Swap payment images on option change

--- javascript/order_payment.js
+++ javascript/order_payment.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+    $("#payment_option > input[type='radio']").each(function(index, item){
+        $(this).click(function(){
+            console.log(this);
 
+            var selectedOption = this.id;
 
+            switch (selectedOption) {
+                case "card":
+                    console.log("카드결제!");
+                    $("#payment_detail_left > img").attr("src","/assets/payment_card.png");
+                    break;
+                case "kakaopay":
+                    $("#payment_detail_left > img").attr("src","/assets/payment_kakaotalk.png");
+                    break;
+                case "point":
+                    $("#payment_detail_left > img").attr("src","/assets/payment_points.png");
+                    break;
+            }
+            
+        });
+    });
+    
 });
